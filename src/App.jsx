0bbc8bb1773cc7d16d4fb1cfe55456b62122ad0f
Oyawe <oyawegoodness@gmail.com>
2023-08-28 14:09:08 -0700

    tours display

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Loading from "./Loading";
+import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
 
@@ -31,6 +32,12 @@ const App = () => {
     );
   }
 
-  return <h2>Tours Starter</h2>;
+  //TODO
+
+  return (
+    <main>
+      <Tours tours={tours} />
+    </main>
+  );
 };
 export default App;
